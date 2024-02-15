c6b2534e2872233753470b6cfe05c51928d13d6b
mirangee <pmrang@live.co.kr>
2024-02-15 17:42:24 +0900

    쿠키 학습

---
+++ 2. web-api/9. 쿠키/popup.js
@@ -0,0 +1,19 @@
+function createCookie (name) {
+  const date = new Date();
+  date.setDate(date.getDate()+1); // 쿠키의 수명을 하루로 설정
+
+  const cookie = `${name} = true;expires=${date.toUTCString()}`; //중간에 세미콜론 꼭 찍을 것
+
+  //쿠키 생성
+  document.cookie = cookie;
+}
+
+function getCookie (name) {
+  const cookies = document.cookie.split('; ');
+  for (let c of cookies) {
+    if (c.search(name) !== -1) {
+      return true;
+    }
+  }
+  return false;
+}
